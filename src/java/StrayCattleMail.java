ed42cb5add16a258df93128e9b5e41ae98968f92
RavinaPuranik <ravinapuranik20@yahoo.in>
2018-04-22 00:33:02 +0530

    Update StrayCattleMail.java

--- src/java/StrayCattleMail.java
+++ src/java/StrayCattleMail.java
@@ -32,9 +32,9 @@ public class StrayCattleMail extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            String to="authority271@gmail.com";  //pass:shreyaravina
-final String from="gomad271@gmail.com";
-final String pass="shreyaravina";
+            String to="<mail_id>";  
+final String from="<mail_id>";
+final String pass="<password>";
 String sub="Complaint regarding Stray Cattles/Dogs";
 String pdesc=request.getParameter("pdesc");
 String states=request.getParameter("states");
